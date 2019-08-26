59f8e6350d02a3f5f050fbc2a473643022590bb2
Arnaud Dufranne <dufrannea@gmail.com>
2019-08-27 00:32:55 +0200

    Adds topological build

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -51,16 +51,47 @@ object Tasks {
     }
   }
 
-  def deps[K, V, I](task: Task[Applicative, K, V]): List[K] = {
+  def deps[K, V](task: Task[Applicative, K, V]): List[K] = {
     import cats.data.Const
 
     task.run(k => Const[List[K], V](List(k))).getConst
+  }
+
+  def topological[K, V, I] = new Build[Applicative, K, V, I] {
+    import scala.language.implicitConversions
+
+    /*
+     * Get all transitive deps in topological order
+     */
+    def transitiveDeps[Ka, Va](key: Ka, tasks: Tasks[Applicative, Ka, Va]): List[Ka] =
+      for {
+        task   <- tasks.get(key).toList
+        dep    <- deps[Ka, Va](task)
+        trDep  <- (transitiveDeps(dep, tasks) ++ List(dep)).toSet.toList // TODO: optimize with visited
+      } yield trDep
+
+    def build(tasks: Tasks[Applicative, K, V], key: K, store: Store[I, K, V]): Store[I, K, V] = {
+      tasks.get(key) match {
+        case None => store
+        case Some(t) =>
+          val depsStore = transitiveDeps[K, V](key, tasks).flatMap({ a =>
+            tasks.get(a).toList.map(_ -> a)
+          }).foldLeft(store)({ case (cs, (currentTask, currentKey)) =>
+            val v = currentTask.run[Id](k => cs.getValue(k))
+            println(s"ran ${currentKey} got ${v}")
+            cs.putValue(currentKey, v)
+          })
+
+          store.putValue(key, t.run[Id](k => depsStore.getValue(k)))
+      }
+    }
   }
   
 }
 
 object Example {
   import Tasks._
+  import scala.language.implicitConversions
 
   // you can define you build task for any Applicative
   //
@@ -72,26 +103,44 @@ object Example {
     def get = key => key match {
       case "B1" => Some(new Task[Applicative, String, Integer]() {
         def run[F[_]](fetch: String => F[Integer])(implicit ec: Applicative[F]): F[Integer] = {
-          Applicative[F].map2(fetch("A1"), fetch("A2"))(_ + _)
+          (fetch("A1"), fetch("A2")).mapN(_ + _)
         }
       })
       case "B2" => Some(new Task[Applicative, String, Integer]() {
         def run[F[_]](fetch: String => F[Integer])(implicit ec: Applicative[F]): F[Integer] = {
-          Applicative[F].map2(fetch("B1"), Applicative[F].pure(2))(_ * _)
+          (fetch("B1"), Applicative[F].pure(2)).mapN(_ * _)
+        }
+      })
+      case _ => None
+    }
+  }
+
+  def workflow2 = new Tasks[Applicative, String, Integer]() {
+    def get = key => key match {
+      case "B1" => Some(new Task[Applicative, String, Integer]() {
+        def run[F[_]](fetch: String => F[Integer])(implicit ec: Applicative[F]): F[Integer] = {
+          (fetch("A1"), fetch("A2")).mapN(_ + _)
+        }
+      })
+      case "B2" => Some(new Task[Applicative, String, Integer]() {
+        def run[F[_]](fetch: String => F[Integer])(implicit ec: Applicative[F]): F[Integer] = {
+          (fetch("A1"), fetch("B1"), Applicative[F].pure(2)).mapN(_ + _ * _)
         }
       })
       case _ => None
     }
   }
 }
+
 object Main {
   def main(args: Array[String]): Unit = {
     import Tasks._
     
     val store: Store[Unit, String, Integer] = SimpleStore((),  (k => if (k == "A1") 10 else 20))
-    val result = Tasks.busy.build(Example.workflow, "B2", store)
-    println(result.getValue("B1"))
+    val result = Tasks.topological.build(Example.workflow2, "B2", store)
+    println(s"B1 ${result.getValue("B1")}")
+    println(s"B2 ${result.getValue("B2")}")
 
-    println(deps(Example.workflow.get("B1").get))
+    println(deps(Example.workflow2.get("B2").get))
   }
 }
